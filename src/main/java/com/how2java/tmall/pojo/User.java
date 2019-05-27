e557379c7386e07e8a69785faaa1c4f41b399844
378887600 <37164607+378887600@users.noreply.github.com>
2019-05-27 08:45:54 +0800

    Update User.java
    更新用户信息表

--- src/main/java/com/how2java/tmall/pojo/User.java
+++ src/main/java/com/how2java/tmall/pojo/User.java
@@ -1,5 +1,7 @@
 package com.how2java.tmall.pojo;
-
+/***
+更新用户信息
+**/
 public class User {
 	private Integer id;
 
@@ -50,6 +52,6 @@ public class User {
 	}
 }
 /**
- * ģ����è��վssm �̳� Ϊhow2j.cn ��Ȩ���� ���̳̽�����ѧϰʹ�ã��������ڷǷ���;���ɴ�����һ�к���뱾վ�޹�
- * ��������ѧϰ������˽�Դ������������ге���ط�������
+ * 模仿天猫整站ssm 教程 为how2j.cn 版权所有 本教程仅用于学习使用，切勿用于非法用途，由此引起一切后果与本站无关
+ * 供购买者学习，请勿私自传播，否则自行承担相关法律责任
  */
